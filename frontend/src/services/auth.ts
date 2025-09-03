d56ba60adf44f9837e8cd917115cc081e46a9405
Aryan Choudhary <aryanc193@gmail.com>
2025-09-03 10:47:49 +0000

    Updated api endpoints wrt professional practice

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -2,15 +2,15 @@ import api from "./api";
 
 // 1. Request OTP to email
 export async function requestOtp(email: string) {
-  return api.post("/auth/otp/request-otp", { email });
+  return api.post("/api/v1/auth/otp/request-otp", { email });
 }
 
 // 2. Verify OTP and get JWT
 export async function verifyOtp(email: string, otp: string) {
-  return api.post("/auth/otp/verify-otp", { email, otp });
+  return api.post("/api/v1/auth/otp/verify-otp", { email, otp });
 }
 
 // 3. Redirect to Google Login
 export function loginWithGoogle() {
-  window.location.href = `${import.meta.env.VITE_API_BASE_URL}/auth/google/login`;
+  window.location.href = `${import.meta.env.VITE_API_BASE_URL}/api/v1/auth/google/login`;
 }
